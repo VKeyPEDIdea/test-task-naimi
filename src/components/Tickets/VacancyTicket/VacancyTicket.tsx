6b4268547912f9c97430aef552f33ae9496d910f
Karyapkin Pavel <pavel.karyapkin@fuse8.online>
2022-03-13 20:30:06 +0600

    add fields to vacancy ticket

--- src/components/Tickets/VacancyTicket/VacancyTicket.tsx
+++ src/components/Tickets/VacancyTicket/VacancyTicket.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import InputSelect from "../../InputSelect";
+import InputText from "../../InputText";
+import InputTextArea from "../../InputTextArea";
 import Row from "../../Row";
 
 const expSelect = {
@@ -25,7 +27,27 @@ const expSelect = {
 };
 
 const VacancyTicket = () => {
+    const [title, setTitle] = useState('');
+    const [conditions, setConditions] = useState('');
+    const [salaryFrom, setSalaryFrom] = useState('');
+    const [salaryTo, setSalaryTo] = useState('');
     const [exp, setExp] = useState('');
+
+    const onTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value);
+    };
+
+    const onConditionsChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setConditions(e.target.value);
+    };
+
+    const onSalaryFromChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSalaryFrom(e.target.value);
+    };
+
+    const onSalaryToChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSalaryTo(e.target.value);
+    };
 
     const onChangeExp = (value: string) => {
         setExp(value);
@@ -33,6 +55,28 @@ const VacancyTicket = () => {
 
     return (
         <div>
+            <Row title='Название вакансии:'>
+                <InputText data={{ id: '1'}}
+                    value={title}
+                    onChange={onTitleChange}/>
+            </Row>
+            <Row title='Обязанности, требования, условия:'>
+                <InputTextArea placeholder='Введите условия и требования к кандидату'
+                    value={conditions}
+                    onChange={onConditionsChange}/>
+            </Row>
+            <Row title='Зарплата:'>
+                <div className='col-2'>
+                    <InputText data={{ id: '3'}}
+                        value={salaryFrom}
+                        placeholder='От'
+                        onChange={onSalaryFromChange}/>
+                    <InputText data={{ id: '4'}}
+                        value={salaryTo}
+                        placeholder='До'
+                        onChange={onSalaryToChange}/>
+                </div>
+            </Row>
             <Row title='Опыт работы:'>
                 <InputSelect data={expSelect}
                     value={exp}
