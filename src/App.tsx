0cc3c3f34405f62db6b10d3918bf1ea4988eff05
Karyapkin Pavel <pavel.karyapkin@fuse8.online>
2022-03-13 20:29:27 +0600

    change button imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import classes from './App.module.scss';
-import Button from './components/Button/Button';
+import Button from './components/Button';
 import Card from './components/Card';
 import ServiceTicket from './components/Tickets/ServiceTicket';
 import VacancyTicket from './components/Tickets/VacancyTicket';
